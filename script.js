9af8930b530276b261c56475baf723abad25c00b
Venght <bvenght@gmail.com>
2024-12-16 19:53:59 +0800

    This changes the content in sectio in html

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const link = document.querySelector("a");
 link.textContent = "Mozilla Developer Network";
 link.href = "https://developer.mozilla.org";
 
+const sect = document.querySelector("section");
+const para = document.querySelector("p");
+para.textContent = "We hope you enjoyed the ride";
+sect.appendChild(para);
