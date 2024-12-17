f5ca74abe607080cb2b11b732d71d594cd8ed8db
Venght <bvenght@gmail.com>
2024-12-17 12:33:50 +0800

    Thei is a mouse event

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-let menu = document.getElementById("menu");
+let btn = document.querySelector('.btn');
 
-menu.addEventListener(`click`, (event) => {
-  let target = event.target;
-
-  switch (target.id) {
-    case "home":
-      console.log("HOme menu item was clicked");
-      break;
-    case "dashboard":
-      console.log("Dashboard menu item was clicked");
-      break;
-    case "report":
-      console.log("Report menu item was clicked");
-      break;
-  }
+btn .addEventListener('click', function () {
+  alert("Mouse clicked");
 });
+
+let clickEvent = new Event('click');
+btn.dispatchEvent(clickEvent);
